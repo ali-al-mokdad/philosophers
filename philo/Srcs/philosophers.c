720792bbcc703c54d0e3da8d9795b10311140fb8
ali <amokdad058@gmail.com>
2025-05-21 16:56:13 +0300

    v2.2 change takes fork prioriti order

--- philo/Srcs/philosophers.c
+++ philo/Srcs/philosophers.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aal-mokd <aal-mokd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 16:15:17 by aal-mokd          #+#    #+#             */
+/*   Updated: 2025/05/21 16:15:17 by aal-mokd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../Include/philosophers.h"
 
 void	ft_philosophers(t_tools *tools)
@@ -14,14 +26,31 @@ void	ft_philosophers(t_tools *tools)
 	ft_monitor(tools);
 }
 
+void	get_fork_order(t_philo *philo, int *first, int *second)
+{
+	if (philo->left_fork < philo->right_fork)
+	{
+		*first = philo->left_fork;
+		*second = philo->right_fork;
+	}
+	else
+	{
+		*first = philo->right_fork;
+		*second = philo->left_fork;
+	}
+}
+
 void	eat(t_philo *philo)
 {
 	t_tools	*tools;
+	int		first;
+	int		second;
 
 	tools = philo->tools;
-	pthread_mutex_lock(&tools->forks[philo->right_fork]);
+	get_fork_order(philo, &first, &second);
+	pthread_mutex_lock(&tools->forks[first]);
 	print_status(tools, philo->id, "has taken a fork");
-	pthread_mutex_lock(&tools->forks[philo->left_fork]);
+	pthread_mutex_lock(&tools->forks[second]);
 	print_status(tools, philo->id, "has taken a fork");
 	print_status(tools, philo->id, "is eating");
 	pthread_mutex_lock(&tools->meal);
@@ -29,9 +58,30 @@ void	eat(t_philo *philo)
 	philo->meal_num++;
 	pthread_mutex_unlock(&tools->meal);
 	usleep(tools->eat_time * 1000);
-	pthread_mutex_unlock(&tools->forks[philo->left_fork]);
-	pthread_mutex_unlock(&tools->forks[philo->right_fork]);
+	pthread_mutex_unlock(&tools->forks[second]);
+	pthread_mutex_unlock(&tools->forks[first]);
 }
+
+
+
+// void	eat(t_philo *philo)
+// {
+// 	t_tools	*tools;
+
+// 	tools = philo->tools;
+// 	pthread_mutex_lock(&tools->forks[philo->right_fork]);
+// 	print_status(tools, philo->id, "has taken a fork");
+// 	pthread_mutex_lock(&tools->forks[philo->left_fork]);
+// 	print_status(tools, philo->id, "has taken a fork");
+// 	print_status(tools, philo->id, "is eating");
+// 	pthread_mutex_lock(&tools->meal);
+// 	philo->last_meal = current_time();
+// 	philo->meal_num++;
+// 	pthread_mutex_unlock(&tools->meal);
+// 	usleep(tools->eat_time * 1000);
+// 	pthread_mutex_unlock(&tools->forks[philo->left_fork]);
+// 	pthread_mutex_unlock(&tools->forks[philo->right_fork]);
+// }
 
 void	*life_cycle(void *arg)
 {
@@ -41,7 +91,7 @@ void	*life_cycle(void *arg)
 	philo = (t_philo *)arg;
 	tools = philo->tools;
 	if (philo->id % 2 == 0)
-		usleep(1500);
+		usleep(1000);
 	while (!check_dead(tools) && tools->philo_num != 1)
 	{
 		eat(philo);
